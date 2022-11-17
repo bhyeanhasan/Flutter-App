0225f2e046d74b21c279c9a93ee92ab2d2f6a04c
Md. Babul Hasan (NoYoN) <bhyean16@cse.pstu.ac.bd>
2022-11-18 00:50:06 +0600

    ok

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp>
+{
+  int ok = 0;
+  var speech = ['To know is to know that you know nothing', 'I go to school','ok bye','good night'];
+
+  void click() {
+
+    setState(() {
+      ok++;
+    });
+    print(ok);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('PSTU CSE-16'),
+        ),
+        body: Column(
+          children: [
+            Text(speech[ok%speech.length]),
+            ElevatedButton(child: Text('bh'), onPressed: click)
+          ],
+        ),
+      ),
+    );
+  }
+}
